a4d2fc4d02bfd508c1da54dd93e0f73a8002d189
Gusnand <gusnanda1170@gmail.com>
2023-08-08 15:47:45 +0800

    fix GET Users, tahap 1, done

--- jwtAuthTest/backend/controllers/UserController.js
+++ jwtAuthTest/backend/controllers/UserController.js
@@ -1,10 +1,10 @@
-import pool from '../config/Database'
+import pool from '../config/Database.js'
 
 export const getUsers = async(req, res) => {
     try {
         pool.query(`SELECT * FROM users`, (error, results) => {
-            if (err) throw error
-            res.status(200).json(results.row)
+            if (error) throw error
+            res.status(200).json(results.rows)
         })
     } catch (error) {
         console.log(error)
--- jwtAuthTest/backend/index.js
+++ jwtAuthTest/backend/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
+import UserRoute from './routes/UserRoute.js'
 
 const app = express()
 // app.use(cors({credentials:true, origin:'http://localhost:3000'}))
 app.use(express.json())
+app.use(UserRoute)
 
 app.listen(5000, () => console.log('Server running...'))
