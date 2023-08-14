78970aae5cb6c6c1b25005027bb418f7b4d404d2
Gusnand <gusnanda1170@gmail.com>
2023-08-14 16:12:39 +0800

    edit button control

--- jwtAuthTest/frontend/src/component/Form.js
+++ jwtAuthTest/frontend/src/component/Form.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {v4 as uuidV4} from 'uuid'
 
 const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
     const updateTodo = (title, id, completed) => {
-        const newTodo = todos.map((todo) => {
+        const newTodo = todos.map((todo) => 
             todo.id === id ? {title, id, completed} : todo
-        })
+        )
         setTodos(newTodo)
         setEditTodo("")
     }
-
+    useEffect(() => {
+        if(editTodo){
+            setInput(editTodo.title)
+        }else{
+            setInput("")
+        }
+    }, [setInput, editTodo])
     const onInputChange = (e) => {
         setInput(e.target.value)
     }
@@ -28,7 +34,7 @@ const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
     <form onSubmit={onFormSubmit}>
         <input type='text' placeholder='Masukkan To-Do...' className='task-input' value={input} required onChange={onInputChange}/>
         <button className='button-add' type='submit'>
-            Add
+            {editTodo ? "Ok" : "Add"}
         </button>
     </form>
   )
