106bd098765b1c0d82d2b3c8a28e4a391c8a6194
Gusnand <gusnanda1170@gmail.com>
2023-08-15 13:38:57 +0800

    localhost db postgreSQL

--- jwtAuthTest/backend/config/Database.js
+++ jwtAuthTest/backend/config/Database.js
@@ -2,7 +2,7 @@ import pkg from 'pg'
 const {Pool} = pkg
 
 const pool = new Pool({
-    host: '172.19.14.118',
+    host: 'localhost',
     port: '5432',
     database: 'todolist',
     user: 'postgres',
